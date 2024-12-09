8b75826ffcdffd1d13466141f730252f14476964
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-12-09 18:19:47 +0545

    hero section is half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      Hello world
-    </div>
-  )
-}
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  );
+};
 export default App;
