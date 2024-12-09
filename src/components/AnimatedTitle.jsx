cb007abd31a122f321b5bc7848e02f397208e128
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-12-09 21:58:17 +0545

    about section is done and now it's time to make some animated title

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,9 @@
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b>o</b>ver the w<b>o</b>rld's l<b>a</b>rgest sh<b>a</b>red <b>a</b>
+      dventure
+    </div>
+  );
+};
+export default AnimatedTitle;
